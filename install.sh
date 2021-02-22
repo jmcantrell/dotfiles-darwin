aa98cd6753663581e98cb83636fb9b08137bb56e
Jeremy Cantrell <jmcantrell@gmail.com>
2021-02-22 02:00:37 -0600

    config changes

--- install.sh
+++ install.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+# install developer tools
+which gcc || xcode-select --install
+
+# install homebrew
+url=https://raw.githubusercontent.com/Homebrew/install/master/install
+which brew || ruby -e "$(curl -fsSL "$url")"
+
+readarray -t packages_brew_formula <./packages/brew-formula
+brew install "${packages_brew_formula[@]}"
+
+readarray -t packages_brew_cask <./packages/brew-cask
+brew install --cask "${packages_brew_cask[@]}"
+
 # screensaver settings
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -float 0.0
