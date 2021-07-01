b991775260a74136815632ced7aa40eb0608693d
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-01 16:06:28 -0500

    Silence errors, too

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-which gcc >/dev/null || xcode-select --install
+which gcc &>/dev/null || xcode-select --install
 
 url=https://raw.githubusercontent.com/Homebrew/install/master/install
-which brew >/dev/null || ruby -e "$(curl -fsSL "$url")"
+which brew &>/dev/null || ruby -e "$(curl -fsSL "$url")"
 
 readarray -t packages_brew_formula <./packages/brew-formula
 brew install "${packages_brew_formula[@]}"
