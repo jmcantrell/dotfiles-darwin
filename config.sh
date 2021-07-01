3909aa4b985a3cfb36790492539c827a6a5c5208
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-01 16:04:32 -0500

    Readability

--- config.sh
+++ config.sh
@@ -19,39 +19,39 @@ defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool t
 defaults write com.apple.menuextra.clock DateFormat "EEE MMM d  h:mm a"
 defaults write com.apple.menuextra.clock IsAnalog -bool false
 
-# show battery percentage free
+# Show battery percentage free.
 defaults write com.apple.menuextra.battery ShowPercent -bool true
 
-# show mounted devices on desktop
+# Show mounted devices on desktop.
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 
-# never restore windows after reboot
+# Never restore windows after reboot.
 defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
 
-# quit printer app after done
+# Quit printer app after done.
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# expanded save/print dialogs by default
+# Expanded save/print dialogs by default.
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-# save to disk by default (not icloud)
+# Save to disk by default (not icloud).
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-# this isn't dangerous and done frequently
+# This isn't dangerous and done frequently.
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# unhide library folder in finder
+# Unhide library folder in finder.
 chflags nohidden ~/Library
 
 if groups | grep -wq admin; then
-    # disable boot sound
-    sudo nvram SystemAudioVolume=" "
+    # Disable boot sound.
+    sudo nvram SystemAudioVolume=%80
 
     # power management settings
     sudo pmset -b sleep 1
@@ -59,7 +59,7 @@ if groups | grep -wq admin; then
     sudo pmset -b displaysleep 1
     sudo pmset -c displaysleep 15
 
-    # add brew installed shells to system
+    # Add brew installed shells to system.
     if ! grep -q /usr/local/bin /etc/shells; then
         printf "%s\n" /usr/local/bin/{bash,zsh} | sudo tee -a /etc/shells >/dev/null
     fi
