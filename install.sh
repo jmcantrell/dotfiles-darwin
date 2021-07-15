20d4244c9776c39c7fe765b05fefabac81731bb1
Jeremy Cantrell <jmcantrell@gmail.com>
2021-07-14 20:39:50 -0500

    Compatibility with older versions of bash

--- install.sh
+++ install.sh
@@ -7,8 +7,5 @@ which gcc &>/dev/null || xcode-select --install
 url=https://raw.githubusercontent.com/Homebrew/install/master/install
 which brew &>/dev/null || ruby -e "$(curl -fsSL "$url")"
 
-readarray -t packages_brew_formula <./packages/brew-formula
-brew install "${packages_brew_formula[@]}"
-
-readarray -t packages_brew_cask <./packages/brew-cask
-brew install --cask "${packages_brew_cask[@]}"
+xargs -r brew install <./packages/brew-formula
+xargs -r brew install --cask <./packages/brew-cask
